6be791c49e394c46e0dedd8af2b44a62984de584
William Wong <compulim@hotmail.com>
2016-03-23 15:06:15 +0800

    WIP: OAuth redirect

--- app.js
+++ app.js
@@ -18,40 +18,40 @@ app.get('/oauth/config', (req, res) => {
   });
 });
 
-app.get('/oauth/redirect', (req, res) => {
-  const
-    query = {
-      client_id: config.clientID,
-      redirect_uri: config.redirectURL,
-      client_secret: config.clientSecret,
-      code: req.query.code,
-      grant_type: 'authorization_code'
-    },
-    queryString = Object.keys(query).map(name => {
-      return `${name}=${encodeURIComponent(query[name])}`;
-    }).join('&');
+// app.get('/oauth/redirect', (req, res) => {
+//   const
+//     query = {
+//       client_id: config.clientID,
+//       redirect_uri: config.redirectURL,
+//       client_secret: config.clientSecret,
+//       code: req.query.code,
+//       grant_type: 'authorization_code'
+//     },
+//     queryString = Object.keys(query).map(name => {
+//       return `${name}=${encodeURIComponent(query[name])}`;
+//     }).join('&');
 
-  console.log(query);
-  console.log(queryString);
+//   console.log(query);
+//   console.log(queryString);
 
-  fetch(
-    'https://login.live.com/oauth20_token.srf',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: queryString
-    }
-  )
-    .then(res => res.json())
-    .then(json => {
-      res.send(json);
-    })
-    .catch(err => {
-      res.status(500).send('failed to exchange code for refresh token');
-    });
-});
+//   fetch(
+//     'https://login.live.com/oauth20_token.srf',
+//     {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//       },
+//       body: queryString
+//     }
+//   )
+//     .then(res => res.json())
+//     .then(json => {
+//       res.send(json);
+//     })
+//     .catch(err => {
+//       res.status(500).send('failed to exchange code for refresh token');
+//     });
+// });
 
 app.use(express.static('html'));
 
